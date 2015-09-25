b657555affd8d7a4f331e102f847748478278e74
Ashley Wright <ashley.wright@meanbee.com>
2015-09-25 09:53:00 +0100

    RR-1: Fixes mismatch in parameter types in observer. Observer methods are passed a Varien_Event_Observer object, observer methods expected a Varien_Event object.

--- app/code/community/Meanbee/RunRate/Model/Observer.php
+++ app/code/community/Meanbee/RunRate/Model/Observer.php
@@ -2,12 +2,12 @@
 
 class Meanbee_RunRate_Model_Observer {
 
-    public function adminhtmlControllerActionPredispatchStart(Varien_Event $observer) {
+    public function adminhtmlControllerActionPredispatchStart(Varien_Event_Observer $observer) {
         if(Mage::app()->getRequest()->getParam('section') == 'cataloginventory' && !Mage::registry('meanbee_runrate_average_run')){
             Mage::register('meanbee_runrate_average_run', Mage::getStoreConfig('cataloginventory/meanbee_runrate/average_run_weeks'));
         }
     }
-    public function adminSystemConfigChangedSectionCataloginventory(Varien_Event $observer) {
+    public function adminSystemConfigChangedSectionCataloginventory(Varien_Event_Observer $observer) {
         $groups = Mage::app()->getRequest()->getPost('groups');
 
         if($groups['meanbee_runrate']['fields']['average_run_weeks'] !== Mage::registry('meanbee_runrate_average_run')) {
